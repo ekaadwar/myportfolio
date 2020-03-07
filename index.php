6f79ef20342bbdfba031352f668ea29fde11ba0b
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-07 22:53:28 +0700

    section Contact selesai, dilanjutkan dengan footer

--- index.php
+++ index.php
@@ -111,18 +111,46 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-sm-12">
+          <div class="col-sm-8 col-sm-offset-2">
             <form>
               <div class="form-group">
                 <label for="nama">Nama</label>
-                <input type="text" id="nama" class="form-control">
+                <input type="text" id="nama" class="form-control" placeholder="masukkan nama">
               </div>
+              <div class="form-group">
+                <label for="email">E-Mail</label>
+                <input type="email" id="email" class="form-control" placeholder="masukkan E-Mail">
+              </div>
+              <div class="form-group">
+                <label for="telp">No. Telp</label>
+                <input type="tel" id="telp" class="form-control" placeholder="masukkan nomor telepon">
+              </div>
+              <select class="form-control">
+                <option>-- Pilih Kategori --</option>
+                <option>Graphic Design</option>
+                <option>Web Development</option>
+              </select>
+              <div class="form-group">
+                <label for="pesan">Pesan</label>
+                <textarea class="form-control" rows="10" placeholder="masukkan pesan"></textarea>  
+              </div>
+              <button type="submit" class="btn btn-primary">Kirim Pesan</button>
             </form>
           </div>
         </div>
       </div>
     </section>
     <!-- /contact -->
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          
+        </div>
+      </div>
+    </footer>
+    <!-- /Footer -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
