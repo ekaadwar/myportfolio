8a0be1729f221a91e2c187ea6b9a4ec0998c6dec
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-08 14:12:56 +0700

    menambahkan kelas 'page-scroll' pada setiap link navbar

--- index.php
+++ index.php
@@ -29,13 +29,13 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a href="#home" class="navbar-brand">Eka Fajhari Adwar</a>
+            <a href="#home" class="navbar-brand page-scroll">Eka Fajhari Adwar</a>
           </div>
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="#about">About</a></li>
-              <li><a href="#portfolio">Portfolio</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li><a href="#about" class="page-scroll">About</a></li>
+              <li><a href="#portfolio" class="page-scroll">Portfolio</a></li>
+              <li><a href="#contact" class="page-scroll">Contact</a></li>
             </ul>
           </div> 
             
