c48546b428bcc1c84dcd05b810fbf6d6d83fdca9
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-10 07:13:23 +0700

    script.js - membuat thumbnail muncul secara satu persatu

--- js/script.js
+++ js/script.js
@@ -36,6 +36,10 @@
 
  	//portfolio
  	if(wScroll > $('.portfolio').offset().top - 250){
- 		$('.portfolio .thumbnail').addClass('muncul');
+ 		$('.portfolio .thumbnail').each(function(i){
+			setTimeout(function(){
+				$('.portfolio .thumbnail').eq(i).addClass('muncul');	
+			}, 300 * i);
+ 		});
  	}
  })
