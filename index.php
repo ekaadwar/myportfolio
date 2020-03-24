9801ef14f20e75a5159d6ea093dba32c2a79af7f
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-24 21:56:43 +0700

    add the social medium in footer

--- index.php
+++ index.php
@@ -168,20 +168,74 @@
 
     <!-- Footer -->
     <footer>
-      <div class="container">
-          <div class="row text-center">
+      <div class="container-fluid">
+          <div class="row">
             <div class="col-sm-12">
-              <p>
-                Alamat : <a href="">
-                  JL PELITA NO. 13, LUBUK SIKAPING, PASAMAN<BR>
-                  SUMATERA BARAT - INDONESIA
-                </a>.
-              </p>
+              <table>
+                <tr>
+                  <td class="td-icon goToRight">
+                    <img src="gambar/icon/location.png" class="icon">
+                  </td>
+                  <td class="goToLeft">
+                    <p>
+                      JL. PELITA NO. 13, LUBUK SIKAPING, PASAMAN<BR>
+                      SUMATERA BARAT - INDONESIA
+                    </p>
+                  </td>
+                  <td class="goToRight">
+                    <a href="">
+                      Eka Fajhari Adwar
+                    </a>
+                  </td>
+                  <td class="td-icon goToLeft">
+                    <img src="gambar/icon/linkedin.png" class="icon">
+                  </td>
+                </tr>
+
+                <tr>
+                  <td class="td-icon goToRight">
+                    <img src="gambar/icon/call.png" class="icon">
+                  </td>
+                  <td class="goToLeft">
+                    <p>
+                      082169150233
+                    </p>
+                  </td>
+                  <td class="goToRight">
+                    <a href="">
+                      https://github.com/ekaadwar
+                    </a>
+                  </td>
+                  <td class="td-icon goToLeft">
+                    <img src="gambar/icon/github.png" class="icon">
+                  </td>
+                </tr>
+
+                <tr>
+                  <td class="td-icon goToRight">
+                    <img src="gambar/icon/email.png" class="icon">
+                  </td>
+                  <td class="goToLeft">
+                    <p>
+                      ekafajhariadwar@gmail.com
+                    </p>
+                  </td>
+                  <td class="goToRight">
+                    <a href="">
+                      @ekaadwar
+                    </a>
+                  </td>
+                  <td class="td-icon goToLeft">
+                    <img src="gambar/icon/ig.png" class="icon">
+                  </td>
+                </tr>
+              </table>
+                    
             </div>
           </div>
           <div class="row text-center">
             <div class="col-sm-12">
-              <p>&copy 2017 | built by. <a href="">Eka Fajhari Adwar</a>.</p>
+              <p>&copy 2020 | built by <a href="">Eka Fajhari Adwar</a>.</p>
             </div>
           </div>
       </div>
