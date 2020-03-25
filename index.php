77da86c266e2c7b810734ed8dfa6dc80035e66c9
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-25 12:06:57 +0700

    menggabungkan semua section menggunakan fungsi include

--- index.php
+++ index.php
@@ -15,6 +15,10 @@
 <?php 
   include "section/atas.php";
   include "section/navbar.php";
-  include "footer";
-  include "bawah.php"
+  include "section/home/jumbotron.php";
+  include "section/home/about.php";
+  include "section/home/portfolio.php";
+  include "section/home/contact.php";
+  include "section/footer.php";
+  include "section/bawah.php"
  ?>
