7e732717fc2b706d648fd0ac80cd46cbe5f089ec
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-26 21:54:30 +0700

    membuat koneksi antara section about dengan database portfolio-about

--- section/home/about.php
+++ section/home/about.php
@@ -1,5 +1,9 @@
 <?php 
-  
+  $sqlAbout = "SELECT * FROM about";
+  $resAbout = $connPortfolio->query($sqlAbout);
+  if($resAbout->num_rows>0){
+    $about = $resAbout->fetch_assoc();
+  }
  ?>
 <section class="about" id="about">
   <div class="container content">
@@ -11,12 +15,12 @@
     </div>
     <div class="row">
       <div class="col-sm-4 col-sm-offset-2">
-        <p class="pKiri">Hai, Anda dapat menyapa saya dengan Eka. Saya adalah lulusan program sarjana dari Jurusan Fisika Fakultas Matematika dan Ilmu Pengetahuan Alam Universitas Andalas, salah satu perguruan tinggi negeri di Kota Padang. Saya sangat suka dalam dunia Web Programming dan desain grafis disamping ilmu fisika.</p>
-        <p class="pKiri">Semasa kuliah, saya mengisi waktu luang dengan berorganisasi yang berlandas pada teknologi. Disana saya terus mengasah ilmu desain grafis yang juga merupakan hobi sejak SMA. Saya mulai mengenal pemrograman website dari salah satu dosen saya yang bernama Bapak Dr. Zulfi. Beliau memperkenalkan salah satu proyek nya yang bernama Appskep. Dari sinilah saya mulai jatuh cinta akan dunia website.</p>
+        <p class="pKiri"><?php echo $about['indo1']; ?></p>
+        <p class="pKiri"><?php echo $about['indo2']; ?></p>
         <hr class="white-section aboutContent">
       </div>
       <div class="col-sm-4">
-        <p class="pKanan">English Version is not found.</p>
+        <p class="pKanan"><?php echo $about['eng1']; ?></p>
       </div>
     </div>
   </div>
