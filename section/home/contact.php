39908d63fa66a6f8a116a1e30161e8c241d58998
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-26 07:51:25 +0700

    memberikan name pada kolom input di section contact

--- section/home/contact.php
+++ section/home/contact.php
@@ -8,27 +8,27 @@
     </div>
     <div class="row">
       <div class="col-sm-8 col-sm-offset-2">
-        <form>
+        <form method="POST" action="">
           <div class="form-group">
             <label for="nama">Nama</label>
-            <input type="text" id="nama" class="form-control" placeholder="masukkan nama">
+            <input type="text" name="nama" id="nama" class="form-control" placeholder="masukkan nama">
           </div>
           <div class="form-group">
             <label for="email">E-Mail</label>
-            <input type="email" id="email" class="form-control" placeholder="masukkan E-Mail">
+            <input type="email" name="email" id="email" class="form-control" placeholder="masukkan E-Mail">
           </div>
           <div class="form-group">
             <label for="telp">No. Telp</label>
-            <input type="tel" id="telp" class="form-control" placeholder="masukkan nomor telepon">
+            <input type="tel" name="hp" id="telp" class="form-control" placeholder="masukkan nomor telepon">
           </div>
-          <select class="form-control">
+          <select name="kategori" class="form-control">
             <option>-- Pilih Kategori --</option>
-            <option>Graphic Design</option>
-            <option>Web Development</option>
+            <option value="Web Development">Web Development</option>
+            <option value="Graphic Design">Graphic Design</option>
           </select>
           <div class="form-group">
             <label for="pesan">Pesan</label>
-            <textarea class="form-control" rows="10" placeholder="masukkan pesan"></textarea>  
+            <textarea name="pesan" class="form-control" rows="10" placeholder="masukkan pesan"></textarea>  
           </div>
           <button type="submit" class="btn btn-warning">Kirim Pesan</button>
         </form>
